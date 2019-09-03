4a619c651ac14750aef202c2e81686c6e0b72916
indri-pr <indri.pr.26@gmail.com>
2019-09-04 04:21:23 +0700

    configuring controllers and application layout

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,2 +1,6 @@
 class SubscriptionsController < ApplicationController
+
+	layout "subscribe"
+
+	
 end
--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,2 +1,6 @@
 module BooksHelper
+	def add_user_to_library? user, book
+		user.libraries.where(user: user, book: book).any?
+	end
+
 end
